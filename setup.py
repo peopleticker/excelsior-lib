bb13e0af5d84f12924004cd72f7ee0d1f354d7ab
Nickolay Lavrinenko <nickolaylavrinenko@gmail.com>
2020-01-10 19:59:02 +0300

    version bump

--- setup.py
+++ setup.py
@@ -33,7 +33,7 @@ def parse_requirements(filename, parent=None):
 
 setup(
     name="excelsior-lib",
-    version="0.0.10",
+    version="0.0.11",
     packages=find_packages(),
     install_requires=list(parse_requirements('requirements.txt'))
 )
